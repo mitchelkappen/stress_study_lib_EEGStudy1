be88df077097b1745bc4577bc13b559bd0e26a92
mitchelkappen <36090493+mitchelkappen@users.noreply.github.com>
2022-04-11 14:54:24 +0200

    Final audio analysis
    All audio reworked. Final dataset loaded in, all figures generated and figs + stats added to manuscript.
    Also transitiond from block to condition.
    Added effect size calculations.
    
    Only need to clean up for relative paths now.

--- scripts/cleanAudioallDataViz.R
+++ scripts/cleanAudioallDataViz.R
@@ -13,6 +13,7 @@ library(lme4)
 library(lmerTest)
 library(pander)
 library(effects)
+library(effectsize)
 
 library(ggpubr)
 library(car)
@@ -31,17 +32,11 @@ pvalues = c() # Create a variable to store all p-values to correct later
 
 BASEPATH <- "D:/Data/EEG_Study_1/aligned_data/features/"
 plotPrefix <- paste0(BASEPATH, "figures/")
-# plotPrefix <- paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/figures/")
 
 data <-
-  as.data.frame(read.csv(paste0(BASEPATH, "dataComplete.csv"))) # This contains everything except for the ones that were too bad -- see Excel drive
-
-data2 <-
   as.data.frame(read_parquet("../loc_data/df_gemaps_func.parquet"))
 
 ##### Clean data up a bit #####
-data <- data[1:(length(data)-8)] # Remove the physiology measures here, are treated in other script
-
 data$participantNum <- as.factor(data$participantNum)
 
 agesex <-
@@ -49,44 +44,20 @@ agesex <-
 
 data <- merge(data, agesex, by = c("participantNum"))
 
-# Add columns for subblock 1-2-3
-data$time[data$fileNum == 0] = 0
-data$time[data$fileNum == 1|data$fileNum == 5] = 1
-data$time[data$fileNum == 2|data$fileNum == 6] = 2
-data$time[data$fileNum == 3|data$fileNum == 7] = 3
-data <- add_column(data[,1:ncol(data)-1], time = data$time, .after = "fileNum")
+# Add final condition names including 'baseline'
+data$condition[data$fileNum == 0] = 'baseline'
+data$condition[data$fileNum == 1|data$fileNum == 2|data$fileNum == 3] = 'Control'
+data$condition[data$fileNum == 5|data$fileNum == 6|data$fileNum == 7] = 'Negative'
+data$condition[data$fileNum == 4|data$fileNum == 8] = 'Rest'
 
-# Add columns for block 0-1-2
-data$block[data$fileNum == 0] = 'baseline'
-data$block[data$fileNum == 1|data$fileNum == 2|data$fileNum == 3] = 'Neutral'
-data$block[data$fileNum == 5|data$fileNum == 6|data$fileNum == 7] = 'Negative'
-data <- add_column(data[,1:ncol(data)-1], block = data$block, .after = "time")
+# Get relevant data
+dataBackup = data # Backup data so we can go back to this whenever
+data = data[data$condition ==  'Control' | data$condition == 'Negative',] # Get only control and negative feedback data (kick out resting blocks)
 
-data$time <- as.factor(data$time)
-data$block <- as.factor(data$block)
-data$block <- relevel(data$block, "Neutral")
-data$block <- relevel(data$block, "baseline")
+data$condition <- as.factor(data$condition)
 data$Sex <- as.factor(data$Sex)
 
-# Take out all NA's values (Resting blocks)
-fullData <- data
-data = data[is.na(data$block) == FALSE, ] # Take out all NA's
-
-# Add F0 baseline corrected
-for(i in unique(data$participantNum)){
-  if(sum(data$participantNum == i & data$time == 0) > 0){
-    if(sum(data$participantNum == i & data$time == 1) > 0){
-      data$F0BaselineCorrected[data$participantNum == i & data$time == 1] = data$F0semitoneFrom27.5Hz_sma3nz_amean[data$participantNum == i & data$time == 1] - data$F0semitoneFrom27.5Hz_sma3nz_amean[data$participantNum == i & data$time == 0]
-    }
-    if(sum(data$participantNum == i & data$time == 2) > 0){
-      data$F0BaselineCorrected[data$participantNum == i & data$time == 2] = data$F0semitoneFrom27.5Hz_sma3nz_amean[data$participantNum == i & data$time == 2] - data$F0semitoneFrom27.5Hz_sma3nz_amean[data$participantNum == i & data$time == 0]
-    }
-    if(sum(data$participantNum == i & data$time == 3) > 0){
-      data$F0BaselineCorrected[data$participantNum == i & data$time == 3] = data$F0semitoneFrom27.5Hz_sma3nz_amean[data$participantNum == i & data$time == 3] - data$F0semitoneFrom27.5Hz_sma3nz_amean[data$participantNum == i & data$time == 0]
-      # break
-    }
-  }
-}
+data = data[data$HNRdBACF_sma3nz_amean > 0, ] # Kick out all the lines with negative HNR, that means the signal is too noisy to trust
 
 # Sample descriptives
 t.first <- data[match(unique(data$participantNum), data$participantNum),] # Create dataframe with one line per unique participant 
@@ -95,16 +66,11 @@ sprintf("Number of Men: %.f. Number of Women: %.f.", sum(t.first$Sex == 'M') , s
 sprintf("Age, Mean: %.2f, SD: %.2f.", mean(t.first$Age) , sd(t.first$Age))
 write.csv(t.first, "../loc_data/temp/IDsSPEECH.csv", row.names = FALSE)
 
-# Get relevant data
-dataBackup = data # Backup data so we can go back to this whenever
-data = data[data$block ==  'Neutral' | data$block == 'Negative',] # Get only block 1 and 2 data
-
-data = data[data$HNRdBACF_sma3nz_amean > 0, ] # Kick out all the lines with negative HNR, that means the signal is too noisy to trust
 ######## Analysis ########
 cbPalette <- c("#56B4E9", "#E69F00") # Colorblind plotting colors
 ####### Speech features #######
 ###### Speech features: F0 ######
-formula <- 'F0semitoneFrom27.5Hz_sma3nz_amean ~ block + Sex + (1|participantNum)' # Declare formula
+formula <- 'F0semitoneFrom27.5Hz_sma3nz_amean ~ condition + Sex + (1|participantNum)' # Declare formula
 
 dataModel = data # Ensure correct data is taken
 rm(d0.1, d0.2, d0.3) # Just to be sure you're not comparing former models for this comparison
@@ -118,17 +84,16 @@ modelNames = c(d0.1,d0.2,d0.3)
 tabel <- cbind(AIC(d0.1), AIC(d0.2), AIC(d0.3))
 chosenModel = modelNames[which(tabel == min(tabel))] # Get model with lowest AIC
 
-Anova(chosenModel[[1]], type = 'III')
+Anova(chosenModel[[1]], type = 'III') #d0.2
 
-emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ block, adjust ="none", type = "response") #we don't adjust because we do this later
+emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ condition, adjust ="none", type = "response") #we don't adjust because we do this later
 emm0.1 <- summary(emmeans0.1)$emmeans
 emmeans0.1$contrasts
 pvalues  = append(pvalues ,summary(emmeans0.1$contrasts)$p.value)
 
 # Effect size
-eff_size(emmeans0.1, sigma = sigma(chosenModel[[1]]), edf = df.residual(chosenModel[[1]]))
 z_to_d(
-  z = c(-2.664),
+  z = c(2.756),
   df_error = 1,
   n = 71
 )
@@ -138,7 +103,7 @@ F0_plot <- F0_plot + annotate('text', x=1.5, y=mean(emm0.1$emmean) + (max(emm0.1
 ggsave(F0_plot, file=paste0(plotPrefix, "Figure_F0.jpeg"), width = 2000, height = 1500, dpi = 300, units = "px")
 
 ###### Speech features: Jitter ######
-formula <- 'jitterLocal_sma3nz_amean ~ block + Sex + (1|participantNum)' # Declare formula
+formula <- 'jitterLocal_sma3nz_amean ~ condition + (1|participantNum)' # Declare formula | Sex dit not contribute significantly
 
 dataModel = data # Ensure correct data is taken
 rm(d0.1, d0.2, d0.3) # Just to be sure you're not comparing former models for this comparison
@@ -154,7 +119,7 @@ chosenModel = modelNames[which(tabel == min(tabel))] # Get model with lowest AIC
 
 Anova(chosenModel[[1]], type = 'III')
 
-emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ block, adjust ="none", type = "response") #we don't adjust because we do this later
+emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ condition, adjust ="none", type = "response") #we don't adjust because we do this later
 emm0.1 <- summary(emmeans0.1)$emmeans
 emmeans0.1$contrasts
 pvalues  = append(pvalues ,summary(emmeans0.1$contrasts)$p.value)
@@ -164,7 +129,7 @@ Jitter_plot <- Jitter_plot + annotate('text', x=1.5, y=mean(emm0.1$emmean) + (ma
 ggsave(Jitter_plot, file=paste0(plotPrefix, "Figure_Jitter.jpeg"), width = 2000, height = 1500, dpi = 300, units = "px")
 
 ###### Speech features: Shimmer ######
-formula <- 'shimmerLocaldB_sma3nz_amean ~ block + Sex + (1|participantNum)' # Declare formula
+formula <- 'shimmerLocaldB_sma3nz_amean ~ condition + Sex + (1|participantNum)' # Declare formula
 
 dataModel = data # Ensure correct data is taken
 rm(d0.1, d0.2, d0.3) # Just to be sure you're not comparing former models for this comparison
@@ -174,23 +139,30 @@ d0.2 <- glmer(formula,data=dataModel, family = Gamma(link = "identity"),glmerCon
 d0.3 <- glmer(formula,data=dataModel, family = inverse.gaussian(link = "identity"),glmerControl(optimizer= "bobyqa", optCtrl = list(maxfun = 100000)),nAGQ = nAGQ)
 
 # Model Selection
-modelNames = c(d0.1) # Other models did not converge
-tabel <- cbind(AIC(d0.1))
+modelNames = c(d0.1,d0.2,d0.3)
+tabel <- cbind(AIC(d0.1), AIC(d0.2), AIC(d0.3))
 chosenModel = modelNames[which(tabel == min(tabel))] # Get model with lowest AIC
 
 Anova(chosenModel[[1]], type = 'III')
 
-emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ block, adjust ="none", type = "response") #we don't adjust because we do this later
+emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ condition, adjust ="none", type = "response") #we don't adjust because we do this later
 emm0.1 <- summary(emmeans0.1)$emmeans
 emmeans0.1$contrasts
 pvalues  = append(pvalues ,summary(emmeans0.1$contrasts)$p.value)
+
+# Effect size
+z_to_d(
+  z = c(2.7881),
+  df_error = 1,
+  n = 71
+)
 
 Shimmer_plot <- audio_pretty_plot(emm0.1, "Shimmer")
 Shimmer_plot <- Shimmer_plot + annotate('text', x=1.5, y=mean(emm0.1$emmean) + (max(emm0.1$emmean) - min(emm0.1$emmean)) / 2, label='**', size=7)
 ggsave(Shimmer_plot, file=paste0(plotPrefix, "Figure_Shimmer.jpeg"), width = 2000, height = 1500, dpi = 300, units = "px")
 
 ###### Speech features: HNR ######
-formula <- 'HNRdBACF_sma3nz_amean ~ block + Sex + (1|participantNum)' # Declare formula
+formula <- 'HNRdBACF_sma3nz_amean ~ condition + Sex + (1|participantNum)' # Declare formula
 
 dataModel = data # Ensure correct data is taken
 rm(d0.1, d0.2, d0.3) # Just to be sure you're not comparing former models for this comparison
@@ -206,17 +178,24 @@ chosenModel = modelNames[which(tabel == min(tabel))] # Get model with lowest AIC
 
 Anova(chosenModel[[1]], type = 'III')
 
-emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ block, adjust ="none", type = "response") #we don't adjust because we do this later
+emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ condition, adjust ="none", type = "response") #we don't adjust because we do this later
 emm0.1 <- summary(emmeans0.1)$emmeans
 emmeans0.1$contrasts
 pvalues  = append(pvalues ,summary(emmeans0.1$contrasts)$p.value)
+
+# Effect size
+z_to_d(
+  z = c(2.858),
+  df_error = 1,
+  n = 71
+)
 
 HNR_plot <- audio_pretty_plot(emm0.1, "HNR")
 HNR_plot <- HNR_plot + annotate('text', x=1.5, y=mean(emm0.1$emmean) + (max(emm0.1$emmean) - min(emm0.1$emmean)) / 2, label='**', size=7)
 ggsave(HNR_plot, file=paste0(plotPrefix, "Figure_HNR.jpeg"), width = 2000, height = 1500, dpi = 300, units = "px")
 
 ###### Speech features: Voiced per sec ######
-formula <- 'VoicedSegmentsPerSec ~ block + (1|participantNum)' # Declare formula
+formula <- 'VoicedSegmentsPerSec ~ condition + (1|participantNum)' # Declare formula
 
 dataModel = data # Ensure correct data is taken
 rm(d0.1, d0.2, d0.3) # Just to be sure you're not comparing former models for this comparison
@@ -232,7 +211,7 @@ chosenModel = modelNames[which(tabel == min(tabel))] # Get model with lowest AIC
 
 Anova(chosenModel[[1]], type = 'III')
 
-emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ block, adjust ="none", type = "response") #we don't adjust because we do this later
+emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ condition, adjust ="none", type = "response") #we don't adjust because we do this later
 emm0.1 <- summary(emmeans0.1)$emmeans
 emmeans0.1$contrasts
 pvalues  = append(pvalues ,summary(emmeans0.1$contrasts)$p.value)
@@ -242,7 +221,7 @@ VoicedSeg_plot <- VoicedSeg_plot + annotate('text', x=1.5, y=mean(emm0.1$emmean)
 ggsave(VoicedSeg_plot, file=paste0(plotPrefix, "Figure_VoicedPerSec.jpeg"), width = 2000, height = 1500, dpi = 300, units = "px")
 
 ###### Speech features: Mean voiced segment length ######
-formula <- 'MeanVoicedSegmentLengthSec ~ block + Sex + (1|participantNum)' # Declare formula
+formula <- 'MeanVoicedSegmentLengthSec ~ condition + Sex + (1|participantNum)' # Declare formula
 
 dataModel = data # Ensure correct data is taken
 rm(d0.1, d0.2, d0.3) # Just to be sure you're not comparing former models for this comparison
@@ -252,13 +231,13 @@ d0.2 <- glmer(formula,data=dataModel, family = Gamma(link = "identity"),glmerCon
 d0.3 <- glmer(formula,data=dataModel, family = inverse.gaussian(link = "identity"),glmerControl(optimizer= "bobyqa", optCtrl = list(maxfun = 100000)),nAGQ = nAGQ)
 
 # Model Selection
-modelNames = c(d0.1) # Other models did not converge
-tabel <- cbind(AIC(d0.1))
+modelNames = c(d0.1,d0.2,d0.3)
+tabel <- cbind(AIC(d0.1), AIC(d0.2), AIC(d0.3))
 chosenModel = modelNames[which(tabel == min(tabel))] # Get model with lowest AIC
 
 Anova(chosenModel[[1]], type = 'III')
 
-emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ block, adjust ="none", type = "response") #we don't adjust because we do this later
+emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ condition, adjust ="none", type = "response") #we don't adjust because we do this later
 emm0.1 <- summary(emmeans0.1)$emmeans
 emmeans0.1$contrasts
 pvalues  = append(pvalues ,summary(emmeans0.1$contrasts)$p.value)
@@ -269,7 +248,7 @@ ggsave(SegLength_plot, file=paste0(plotPrefix, "Figure_MeanVoicedLength.jpeg"), 
 
 ####### Self-reports #######
 ###### Self-reports: Valence ######
-formula <- 'valence ~ block + (1|participantNum)' # Declare formula
+formula <- 'valence ~ condition + (1|participantNum)' # Declare formula
 
 dataModel = data # Ensure correct data is taken
 rm(d0.1, d0.2, d0.3) # Just to be sure you're not comparing former models for this comparison
@@ -285,16 +264,20 @@ chosenModel = modelNames[which(tabel == min(tabel))] # Get model with lowest AIC
 
 Anova(chosenModel[[1]], type = 'III')
 
-emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ block, adjust ="fdr", type = "response") #we don't adjust because we do this later
+emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ condition, adjust ="fdr", type = "response") #we don't adjust because we do this later
 emm0.1 <- summary(emmeans0.1)$emmeans
 emmeans0.1$contrasts
+
+# Effect size
+param_tab <- parameters::model_parameters(chosenModel[[1]])
+t_to_d(param_tab$t[2], param_tab$df_error[2])
 
 valence_plot <- audio_pretty_plot(emm0.1, "Valence")
 valence_plot <- valence_plot + annotate('text', x=1.5, y=mean(emm0.1$emmean) + (max(emm0.1$emmean) - min(emm0.1$emmean)) / 4, label='***', size=7)
 ggsave(valence_plot, file=paste0(plotPrefix, "Figure_Valence.jpeg"), width = 2000, height = 1500, dpi = 300, units = "px")
 
 ###### Self-reports: Arousal ######
-formula <- 'arousal ~ block + (1|participantNum)' # Declare formula
+formula <- 'arousal ~ condition + (1|participantNum)' # Declare formula
 
 dataModel = data # Ensure correct data is taken
 rm(d0.1, d0.2, d0.3) # Just to be sure you're not comparing former models for this comparison
@@ -310,9 +293,16 @@ chosenModel = modelNames[which(tabel == min(tabel))] # Get model with lowest AIC
 
 Anova(chosenModel[[1]], type = 'III')
 
-emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ block, adjust ="fdr", type = "response") #we don't adjust because we do this later
+emmeans0.1 <- emmeans(chosenModel[[1]], pairwise ~ condition, adjust ="fdr", type = "response") #we don't adjust because we do this later
 emm0.1 <- summary(emmeans0.1)$emmeans
 emmeans0.1$contrasts
+
+# Effect size
+z_to_d(
+  z = c(2.116),
+  df_error = 1,
+  n = 71
+)
 
 arousal_plot <- audio_pretty_plot(emm0.1, "Arousal")
 arousal_plot <- arousal_plot + annotate('text', x=1.5, y=mean(emm0.1$emmean) + (max(emm0.1$emmean) - min(emm0.1$emmean)) / 2, label='*', size=7)
